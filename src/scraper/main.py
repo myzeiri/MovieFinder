0db37a59443738043742c9b5c703fb0f930a6b42
Martin Yzeiri <yzeiri.1@osu.edu>
2017-10-21 17:22:24 -0400

    Added options for the user to sort by genre.

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -9,15 +9,24 @@ from scrape_text import get_page_text
 from get_title import get_title
 from create_movie_list import scrape_movies, create_movie_list
 
-scraped_movies = scrape_movies()
+print("Welcome to the Gateway Film Scraper.")
+print("Enter the genres you are interested in seperated by commas.")
 
+genres_input = input()
+genres = genres_input.split(",")
+
+# Get the movie information
+scraped_movies = scrape_movies()
 movie_list = create_movie_list(scraped_movies)
 
-
-file = open("output.txt", "w")
+users_picks = []
 
 for m in movie_list:
+	for g in genres:
+		if g in m.genre:
+			users_picks.append(m)
+			break
 
 
-file.close()
-
+for m in users_picks:
+	m.prettyPrint()
