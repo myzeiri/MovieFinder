740b4ed469847e074b47973fea4afcf029ae68ae
Edmonk Shek <edmondshek11@gmail.com>
2017-10-21 12:09:16 -0400

    Updated movie class and added add_movie_details function

--- src/movie.py
+++ src/movie.py
@@ -1,9 +1,9 @@
 class Movie: 
 	#This is the movie class which will contain title, opening date, genre, rating#
-	def __init__(self, title, opening_date, genre, rating, runtime):
+
+	def __init__(self, title, opening_date):
 		self.title = title
 		self.opening_date = opening_date
-		self.genre = genre
-		self.rating = rating
-		self.runtime = runtime
-
+		self.genre = "genre"
+		self.rating = "rating"
+		
---
+++ src/scraper/add_movie_details.py
@@ -0,0 +1,19 @@
+#Figure out how to use api
+#Figure out how to use requests
+
+import json
+import urllib2
+
+base_url = ""
+
+def add_movie_details(movie_array):
+	#need while loop to create a movie using the title array and api
+	for movie in movie_array:
+		title = movie.title
+		#have to use the title with whatever api we are using and get a map
+		movie_data = json.load(urllib2.urlopen(base_url+title))
+		#Use map to populate movie object
+		movie.genre = movie_data["data"]["genre"]
+		movie.rating = movie_data["data"]["rating"]
+
+	return movie_array
