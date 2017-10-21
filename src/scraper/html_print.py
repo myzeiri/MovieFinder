d5f26c92a36c7f79bda78ae78c91350804ec87de
Edmonk Shek <edmondshek11@gmail.com>
2017-10-21 17:22:45 -0400

    added html_print

---
+++ src/scraper/html_print.py
@@ -0,0 +1,21 @@
+
+def html_print(movie_list):
+	file = open("output.txt", "w")
+	file.write("<html>")
+	file.write("<head>")
+	file.write("<title>Movies out soon</title>")
+	for m in movie_list:
+		file.write("<h1>Title: " + m.original_title + "</h1>")
+		file.write("<h2>Release Date: " + m.opening_date + "</h2>")
+		if (m.genre == "genre"):
+			file.write("<h2>Genre: genre unavailable</h2>")
+		else:
+			file.write("<h2>Genre: " + m.genre + "</h2>")
+		if (m.rating == "rating"):
+			file.write("<h2>Rating: rating unavailable</h2>")
+		else:
+			file.write("<h2>Rating: " + m.rating + "</h2>")
+		file.write("<br><br>")
+	file.write("</head>")
+	file.write("</html>")
+	file.close()	
--- src/scraper/main.py
+++ src/scraper/main.py
@@ -8,16 +8,9 @@ from get_next_line import get_next_line
 from scrape_text import get_page_text
 from get_title import get_title
 from create_movie_list import scrape_movies, create_movie_list
+from html_print import html_print
 
 scraped_movies = scrape_movies()
 
 movie_list = create_movie_list(scraped_movies)
-
-
-file = open("output.txt", "w")
-
-for m in movie_list:
-
-
-file.close()
-
+html_print(movie_list)
