dfdf0511c5f77b063fb169e93b9d6271590d0f00
Edmonk Shek <edmondshek11@gmail.com>
2017-10-21 11:13:55 -0400

    Created Movie Class

---
+++ src/movie.py
@@ -0,0 +1,9 @@
+class Movie: 
+	#This is the movie class which will contain title, opening date, genre, rating#
+	def __init__(self, title, opening_date, genre, rating, runtime):
+		self.title = title
+		self.opening_date = opening_date
+		self.genre = genre
+		self.rating = rating
+		self.runtime = runtime
+
